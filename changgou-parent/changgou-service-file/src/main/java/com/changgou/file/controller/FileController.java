c458b4402ba69f2a172a0ce76bf3b8e77a584f84
luoshuibing@126.com <luoshuibing@126.com>
2024-05-19 19:37:58 +0800

    模板服务、规格服务、参数服务、分类服务

--- changgou-parent/changgou-service-file/src/main/java/com/changgou/file/controller/FileController.java
+++ changgou-parent/changgou-service-file/src/main/java/com/changgou/file/controller/FileController.java
@@ -2,14 +2,13 @@ package com.changgou.file.controller;
 
 import com.changgou.file.util.FastDFSClient;
 import com.changgou.file.util.FastDFSFile;
+import entity.Result;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/file")
 @CrossOrigin
 public class FileController {
 
@@ -18,16 +17,19 @@ public class FileController {
      * @return
      */
     @PostMapping(value = "/upload")
-    public String upload(@RequestParam("file") MultipartFile file) throws Exception {
-        //封装一个FastDFSFile
-        FastDFSFile fastDFSFile = new FastDFSFile(
-                file.getOriginalFilename(), //文件名字
-                file.getBytes(),            //文件字节数组
-                StringUtils.getFilenameExtension(file.getOriginalFilename()));//文件扩展名
+    public Result<String> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        // 封装一个FastDFSFile
+        FastDFSFile fastDFSFile = new FastDFSFile(file.getOriginalFilename(), // 文件名字
+                file.getBytes(),            // 文件字节数组
+                StringUtils.getFilenameExtension(file.getOriginalFilename()));// 文件扩展名
 
-        //文件上传
+        // 文件上传
         String[] uploads = FastDFSClient.upload(fastDFSFile);
-        //组装文件上传地址
-        return FastDFSClient.getTrackerUrl()+"/"+uploads[0]+"/"+uploads[1];
+        // 组装文件上传地址
+        String fileUrl = FastDFSClient.getTrackerUrl() + "/" + uploads[0] + "/" + uploads[1];
+        return Result.ok("上传成功", fileUrl);
     }
+
+
+
 }
